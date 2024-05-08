55818e390bfc49986c2ef91ef907793e182cbb43
joaovictor720 <silvajoao720@gmail.com>
2024-05-08 15:40:54 -0300

    só pra não perder isso tudo, mas os IoUs tão aí

--- predict.py
+++ predict.py
@@ -4,24 +4,29 @@ import numpy as np
 import tifffile as tiff
 import matplotlib.pyplot as plt
 import cv2
+import scipy
 
 def bb_intersection_over_union(boxA, boxB):
-	# determine the (x, y)-coordinates of the intersection rectangle
+	# https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
+	# ^^ corrected.
+		
+	# Determine the (x, y)-coordinates of the intersection rectangle
 	xA = max(boxA[0], boxB[0])
 	yA = max(boxA[1], boxB[1])
 	xB = min(boxA[2], boxB[2])
 	yB = min(boxA[3], boxB[3])
-	# compute the area of intersection rectangle
-	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-	# compute the area of both the prediction and ground-truth
-	# rectangles
+
+	interW = xB - xA + 1
+	interH = yB - yA + 1
+
+	# Correction: reject non-overlapping boxes
+	if interW <=0 or interH <=0 :
+		return -1.0
+
+	interArea = interW * interH
 	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
 	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-	# compute the intersection over union by taking the intersection
-	# area and dividing it by the sum of prediction + ground-truth
-	# areas - the interesection area
 	iou = interArea / float(boxAArea + boxBArea - interArea)
-	# return the intersection over union value
 	return iou
 
 def cv_bb_to_iou_bb(bb):
@@ -51,7 +56,7 @@ def load_image(img_id, base_dir="/home/jv/Documents/38cloud/38-Cloud_training/tr
 
 # Retorna a imagem original com os retângulos desenhados, e as coordenadas dos retângulos
 # da máscara e do ground-truth
-def handle_bbs(mask_id, gt_id, original_id, base_dir="/home/jv/Documents/38cloud/38-Cloud_training", extension="TIF"):
+def handle_bbs_from_image(mask_id, gt_id, original_id, base_dir="/home/jv/Documents/38cloud/38-Cloud_training", extension="TIF"):
 	mask_path = f"{base_dir}/masks/mask{mask_id}.{extension}"
 	gt_path = f"{base_dir}/train_gt/gt{gt_id}.{extension}"
 	original_path = f"{base_dir}/train_red/red{original_id}.{extension}"
@@ -67,25 +72,90 @@ def handle_bbs(mask_id, gt_id, original_id, base_dir="/home/jv/Documents/38cloud
 
 	# Desenhando os bounding boxes da máscara gerada
 	mask_boxes = []
+	mask_boxes_cv = []
 	for contour in mask_contours:
-		x, y, w, h = cv2.boundingRect(contour)
-		mask_boxes.append(cv_bb_to_iou_bb(cv2.boundingRect(contour)))
-		cv2.rectangle(original_image, (x, y), (x + w, y + h), (0, 0, 255), 1)
+		bb = cv2.boundingRect(contour)
+		mask_boxes_cv.append(bb)
+		mask_boxes.append(cv_bb_to_iou_bb(bb))
 	
 	# Desenhando os bounding boxes do gabarito
 	gt_boxes = []
+	gt_boxes_cv = []
 	for contour in gt_contours:
-		x, y, w, h = cv2.boundingRect(contour)
-		gt_boxes.append(cv_bb_to_iou_bb(cv2.boundingRect(contour)))
-		cv2.rectangle(original_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-	return original_image, mask_boxes, gt_boxes
+		bb = cv2.boundingRect(contour)
+		gt_boxes_cv.append(bb)
+		gt_boxes.append(cv_bb_to_iou_bb(bb))
 
-def get_ious(mask_boxes, gt_boxes):
-	ious = []
-	for i in range(min(len(mask_boxes), len(gt_boxes))):
-		print(f"Calculando ({mask_boxes[i]}) / ({gt_boxes[i]})")
-		ious.append(bb_intersection_over_union(mask_boxes[i], gt_boxes[i]))
-	return ious
+	# Obtendo os matches e IoUs entre os bounding boxes
+	matched_gt_idxs, matched_mask_idxs, ious, pred_truths = match_bbs(np.array(mask_boxes), np.array(gt_boxes))
+
+	# Desenhando os pares previsão-gt na imagem
+	for i in range(len(matched_gt_idxs)):
+		bb = mask_boxes_cv[matched_mask_idxs[i]]
+		cv2.rectangle(original_image, (bb[0], bb[1]), (bb[0] + bb[2], bb[1] + bb[3]), (0, 255, 0), 1) # Desenhando um bb de pred
+		bb = gt_boxes_cv[matched_gt_idxs[i]]
+		cv2.rectangle(original_image, (bb[0], bb[1]), (bb[0] + bb[2], bb[1] + bb[3]), (0, 0, 255), 2) # Desenhando um bb de gt
+
+	return original_image, ious, pred_truths
+
+def match_bbs(gt_boxes, pred_boxes, IOU_THRESH=0.5):
+    '''
+    Given sets of true and predicted bounding-boxes,
+    determine the best possible match.
+    Parameters
+    ----------
+    bbox_gt, bbox_pred : N1x4 and N2x4 np array of bboxes [x1,y1,x2,y2]. 
+      The number of bboxes, N1 and N2, need not be the same.
+    
+    Returns
+    -------
+    (idxs_true, idxs_pred, ious, labels)
+        idxs_true, idxs_pred : indices into gt and pred for matches
+        ious : corresponding IOU value of each match
+        labels: vector of 0/1 values for the list of detections
+    '''
+    n_true = gt_boxes.shape[0]
+    n_pred = pred_boxes.shape[0]
+    MAX_DIST = 1.0
+    MIN_IOU = 0.0
+
+    # NUM_GT x NUM_PRED
+    iou_matrix = np.zeros((n_true, n_pred))
+    for i in range(n_true):
+        for j in range(n_pred):
+            iou_matrix[i, j] = bb_intersection_over_union(gt_boxes[i,:], pred_boxes[j,:])
+
+    if n_pred > n_true:
+      # there are more predictions than ground-truth - add dummy rows
+      diff = n_pred - n_true
+      iou_matrix = np.concatenate( (iou_matrix, 
+                                    np.full((diff, n_pred), MIN_IOU)), 
+                                  axis=0)
+
+    if n_true > n_pred:
+      # more ground-truth than predictions - add dummy columns
+      diff = n_true - n_pred
+      iou_matrix = np.concatenate( (iou_matrix, 
+                                    np.full((n_true, diff), MIN_IOU)), 
+                                  axis=1)
+
+    # call the Hungarian matching
+    idxs_true, idxs_pred = scipy.optimize.linear_sum_assignment(1 - iou_matrix)
+
+    if (not idxs_true.size) or (not idxs_pred.size):
+        ious = np.array([])
+    else:
+        ious = iou_matrix[idxs_true, idxs_pred]
+
+    # remove dummy assignments
+    sel_pred = idxs_pred<n_pred
+    idx_pred_actual = idxs_pred[sel_pred] 
+    idx_gt_actual = idxs_true[sel_pred]
+    ious_actual = iou_matrix[idx_gt_actual, idx_pred_actual]
+    sel_valid = (ious_actual > IOU_THRESH)
+    label = sel_valid.astype(int)
+
+    return idx_gt_actual[sel_valid], idx_pred_actual[sel_valid], ious_actual[sel_valid], label 
 
 # Load the trained model
 original_image_id = "_patch_267_13_by_15_LC08_L1TP_029040_20160720_20170222_01_T1"
@@ -121,10 +191,10 @@ matrix_3channel[:, :, 2] = binarized_matrix
 base_dir = "/home/jv/Documents/38cloud/38-Cloud_training"
 cv2.imwrite(f"{base_dir}/masks/mask{original_image_id}.TIF", matrix_3channel)
 
-bounded_image, mask_bbs, gt_bbs = handle_bbs(gt_id=original_image_id, mask_id=original_image_id, original_id=original_image_id)
+bounded_image, ious, pred_truths = handle_bbs_from_image(gt_id=original_image_id, mask_id=original_image_id, original_id=original_image_id)
 
 print("IoUs:")
-print(get_ious(mask_bbs, gt_bbs))
+print(ious)
 
 # Display the image with bounding boxes
 print("SALVANDO A IMAGEM COM OS BOUNDING BOXES PREVISTOS")
